40959fae9caa18f8047830a1db8983cbb4a0803f
Omkar <omkar.pathade2011@gmail.com>
2023-07-25 21:03:13 +0530

    Added Entities

---
+++ src/main/java/com/example/rolebasedauth/entities/Product.java
@@ -0,0 +1,21 @@
+package com.example.rolebasedauth.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private Double price;
+    @ManyToOne
+    private User user;
+}
